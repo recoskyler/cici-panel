f2d9971d72e8cce1701a33b5f16bc4217edfa0c3
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-09-04 20:19:15 +0300

    Added Lucia types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,29 @@
+/* eslint-disable no-unused-vars */
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-  // eslint-disable-next-line no-unused-vars
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
 	}
+}
+
+/// <reference types="lucia" />
+declare namespace Lucia {
+  type Auth = import('$lib/server/lucia').Auth;
+  type DatabaseUserAttributes = {
+    email: string;
+    verified: boolean;
+    displayName: string;
+    firstName: string;
+    lastName: string;
+    mobile: string;
+  };
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  type DatabaseSessionAttributes = {};
 }
 
 export {};
