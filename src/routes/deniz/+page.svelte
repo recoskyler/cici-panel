d68bf4cd4407ce6c29f329653d3d9f5859db40be
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-09-04 20:23:13 +0300

    Added Deniz's page

---
+++ src/routes/deniz/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>🐴 ❤</title>
+</svelte:head>
+
+<a href="/" class="w-full py-10 px-5 bg-white flex items-center justify-center min-h-screen">
+  <div class="center text-black text-center">
+    <p>
+      <i>
+        In loving memory of Deniz, my childhood and best friend.
+        <br /><br />
+        1999 - 2023
+      </i>
+    </p>
+  </div>
+</a>
